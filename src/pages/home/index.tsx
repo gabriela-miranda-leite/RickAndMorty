48ccd20bcf9da09b40248484e681f337e2f02a7d
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-23 13:24:43 -0300

    feat: create home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+import {RootStackParamList} from '../../routes';
+
 import * as S from './styles';
 
+type HomeScreenProps = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+
 export const Home = () => {
+  const navigation = useNavigation<HomeScreenProps>();
+
   return (
     <S.Container>
-      <S.Background source={require('../../assets/home.png')} />
+      <S.Background source={require('../../assets/home.png')}>
+        <S.Logo source={require('../../assets/logo.png')} />
+
+        <S.ButtonToHome onPress={() => navigation.navigate('Feed')}>
+          <S.TextButton>Prosseguir</S.TextButton>
+          <S.ArrowIcon name="arrow-forward" size={22} color="#FFFFFF" />
+        </S.ButtonToHome>
+      </S.Background>
     </S.Container>
   );
 };

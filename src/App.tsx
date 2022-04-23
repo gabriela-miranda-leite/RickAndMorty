daeff7bdf56e533446c1cd74d6218a2e9ae51b13
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-23 11:37:15 -0300

    refactor: update path again

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import {StatusBar} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
 
-import {Home} from '../pages';
+import {Home} from './pages';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
     </NavigationContainer>
   );

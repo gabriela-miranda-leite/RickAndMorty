03405cc85dd0413117c69552cbc0550afc7c3081
gabrielamiranda11.10 <gabrielamiranda11.10@gmail.com>
2022-04-22 22:40:21 -0300

    feat: create styles global

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    primary: '#1E2047',
+    text: {
+      dark: '#333333',
+      light: '#FFFFFF',
+    },
+    status: {
+      alive: '#4AC82A',
+      dead: '#E91337',
+      unkown: '#FF9000',
+    },
+  },
+  fonts: {
+    regular: 'Poppins-Regular',
+    semiBold: 'Poppins-SemiBold',
+  },
+};

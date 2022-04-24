033d9be436a9d3bbf2d99338c827995face342e0
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-23 22:02:37 -0300

    feat: create pagination

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,18 +1,116 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
 
-import {InputSearch} from '../../components';
+import api from '../../services/api';
+
+import {InputSearch, Card} from '../../components';
 
 import * as S from './styles';
 
+export interface CharacterProps {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  gender: string;
+  origin: {
+    name: string;
+  };
+  location: {
+    name: string;
+  };
+  image: string;
+}
+
 export const Feed = () => {
+  const [pageStatus, setPageStatus] = useState('idle');
+  const [dataCharacters, setDataCharacters] = useState<CharacterProps[]>([]);
+  const [totalCharacters, setTotalCharacters] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>();
+  const [page, setPage] = useState<number>(2);
+
+  const nextPage = useCallback(async () => {
+    if (page === totalPages) {
+      setTotalCharacters(0);
+      setPage(1);
+      return;
+    }
+
+    setPageStatus('loading');
+    setPage(page + 1);
+
+    const response = await api.get(`/character/?page=${page}`);
+
+    if (response.status !== 200) {
+      setPageStatus('error');
+
+      setTotalCharacters(0);
+      setPage(1);
+      return;
+    }
+
+    setPageStatus('sucess');
+
+    setDataCharacters(rest => [...rest, ...response.data.results]);
+    setTotalPages(response.data.info.pages);
+    setTotalCharacters(response.data.info.count);
+  }, [page, totalPages]);
+
+  useEffect(() => {
+    const getAllCharacters = async () => {
+      setPageStatus('loading');
+      const response = await api.get('/character/');
+
+      if (response.status !== 200) {
+        setPageStatus('error');
+
+        setTotalCharacters(0);
+        setPage(1);
+        return;
+      }
+
+      setPageStatus('sucess');
+
+      setDataCharacters(response.data.results);
+      setTotalPages(response.data.info.pages);
+      setTotalCharacters(response.data.info.count);
+    };
+
+    getAllCharacters();
+  }, []);
+
   return (
     <S.Container>
       <S.HeaderContainer>
         <S.Title>Listagem</S.Title>
-        <S.NumberOfCharacters> 64 personagens</S.NumberOfCharacters>
+        <S.NumberOfCharacters>
+          {totalCharacters} personagens
+        </S.NumberOfCharacters>
       </S.HeaderContainer>
 
       <InputSearch placeholder="Busque por um personagem" />
+
+      <FlatList
+        style={{padding: 15}}
+        data={dataCharacters}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item: character}) => <Card character={character} />}
+        showsVerticalScrollIndicator={false}
+        onEndReachedThreshold={0.1}
+        onEndReached={nextPage}
+        ListEmptyComponent={() => (
+          <S.EmptyList>
+            <S.EmptyText>Personagem não encontrado</S.EmptyText>
+          </S.EmptyList>
+        )}
+        ListFooterComponent={
+          pageStatus === 'loading' ? (
+            <S.Loading size="small" color="#1e2047" />
+          ) : (
+            <S.FooterComponent />
+          )
+        }
+      />
     </S.Container>
   );
 };

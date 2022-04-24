618e2786b7c4d9efd83aef1c7cbe2c9f018e10b8
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-23 22:04:45 -0300

    feat: Empty List styles

--- src/pages/feed/styles.ts
+++ src/pages/feed/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components/native';
 import {theme} from '../../styles/theme';
 
+import {FlatList} from 'react-native';
+
 export const Container = styled.View`
   flex: 1;
   background-color: ${theme.colors.background};
@@ -27,7 +29,27 @@ export const Title = styled.Text`
 export const NumberOfCharacters = styled.Text.attrs({
   numberOfLines: 1,
 })`
-  font-family: ${theme.fonts.semiBold};
+  font-family: ${theme.fonts.regular};
   color: ${theme.colors.background};
   font-size: 14px;
+`;
+
+export const CharactersList = styled(FlatList)``;
+
+export const EmptyList = styled.View`
+  width: 100%;
+`;
+
+export const EmptyText = styled.Text`
+  color: ${theme.colors.text.dark};
+  font-size: 16px;
+`;
+
+export const Loading = styled.ActivityIndicator`
+  margin: 15px 0;
+`;
+
+export const FooterComponent = styled.View`
+  width: 100%;
+  height: 15px;
 `;

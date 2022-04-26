a9bad35c5cdb5d054743c1964da74fcd15cd5bd7
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-26 20:34:00 -0300

    feat: adding header show in profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Profile, Home, Feed} from '../pages';
+import {CharacterProps} from '../pages/feed';
 
 export type RootStackParamList = {
   Home: undefined;
   Feed: undefined;
-  Profile: undefined;
+  Profile: {
+    character: CharacterProps;
+  };
 };
 
 const Navigation = createNativeStackNavigator<RootStackParamList>();
@@ -16,10 +19,19 @@ const Routes: React.FC = () => {
     <Navigation.Navigator
       screenOptions={{
         headerShown: false,
+        headerTransparent: true,
+        headerTintColor: '#1E2047',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
       }}>
       <Navigation.Screen name="Home" component={Home} />
       <Navigation.Screen name="Feed" component={Feed} />
-      <Navigation.Screen name="Profile" component={Profile} />
+      <Navigation.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: true, title: ''}}
+      />
     </Navigation.Navigator>
   );
 };

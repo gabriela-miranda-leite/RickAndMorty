a3fea2362d532f2f4bfea1820edcbd2464fbf9dd
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-26 20:32:52 -0300

    feat: create profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,83 @@
 import React from 'react';
 
-export const Profile = () => {
-  return <></>;
+import {Linking, ScrollView} from 'react-native';
+
+import {useNavigation, RouteProp} from '@react-navigation/native';
+
+import {CharacterProps} from '../../pages/feed';
+
+import * as S from './styles';
+interface Props {
+  route: RouteProp<
+    {
+      params: {
+        character: CharacterProps;
+      };
+    },
+    'params'
+  >;
+}
+
+export const Profile: React.FC<Props> = ({route}) => {
+  const navigation = useNavigation();
+  const {character} = route.params;
+
+  const searchGoogle = () => {
+    return Linking.openURL(
+      `https://www.google.com.br/search?q=Rick%20and%20Morty%20${character?.name}`,
+    );
+  };
+
+  return (
+    <S.Container>
+      <S.ContentImage>
+        <S.CharacterImage source={{uri: character.image}} />
+      </S.ContentImage>
+
+      <ScrollView>
+        <S.Content>
+          <S.Name>{character.name}</S.Name>
+
+          <S.Details>
+            <S.DetailsContent>
+              <S.Label>Species:</S.Label>
+              <S.Info>{character.species}</S.Info>
+            </S.DetailsContent>
+
+            <S.DetailsContent>
+              <S.Label>Gender:</S.Label>
+              <S.Info>{character.gender}</S.Info>
+            </S.DetailsContent>
+          </S.Details>
+
+          <S.Separator />
+
+          <S.LocationContent>
+            <S.Label>Location:</S.Label>
+            <S.Info>{character.location.name}</S.Info>
+          </S.LocationContent>
+
+          <S.Separator />
+
+          <S.Details>
+            <S.DetailsContent>
+              <S.Label>Origin:</S.Label>
+              <S.Info>{character.origin.name}</S.Info>
+            </S.DetailsContent>
+
+            <S.DetailsContent>
+              <S.Label>Status:</S.Label>
+              <S.Status status={character.status}>{character.status}</S.Status>
+            </S.DetailsContent>
+          </S.Details>
+        </S.Content>
+      </ScrollView>
+
+      <S.ButtonGoogle>
+        <S.ButtonContent onPress={() => searchGoogle()}>
+          Buscar no Google
+        </S.ButtonContent>
+      </S.ButtonGoogle>
+    </S.Container>
+  );
 };
--- src/pages/profile/styles.ts
+++ src/pages/profile/styles.ts
@@ -1,3 +1,82 @@
 import styled from 'styled-components/native';
+import {theme} from '../../styles/theme';
 
-export const container = styled.View``;
+interface StatusProps {
+  status: 'Alive' | 'Dead' | 'Unknown';
+}
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const ContentImage = styled.View`
+  height: 50%;
+  position: relative;
+`;
+
+export const CharacterImage = styled.Image`
+  height: 100%;
+`;
+
+export const Content = styled.View`
+  padding: 20px;
+  height: 40%;
+`;
+
+export const Name = styled.Text.attrs({numberOfLine: 1})`
+  color: ${theme.colors.text.dark};
+  font-family: ${theme.fonts.semiBold};
+  font-size: 24px;
+  margin-bottom: 30px;
+`;
+
+export const Label = styled.Text.attrs({numberOfLine: 1})`
+  color: ${theme.colors.text.dark};
+  font-family: ${theme.fonts.regular};
+  font-size: 16px;
+`;
+
+export const Info = styled.Text.attrs({numberOfLine: 1})`
+  color: ${theme.colors.text.dark};
+  font-family: ${theme.fonts.semiBold};
+  font-size: 16px;
+`;
+
+export const Details = styled.View`
+  flex-direction: row;
+  width: 100%;
+`;
+
+export const DetailsContent = styled.View`
+  width: 50%;
+`;
+
+export const LocationContent = styled.View`
+  width: 100%;
+`;
+
+export const Separator = styled.View`
+  margin-bottom: 30px;
+`;
+
+export const Status = styled.Text<StatusProps>`
+  color: ${theme.colors.text.dark};
+  font-family: ${theme.fonts.semiBold};
+  font-size: 16px;
+  color: ${({status}) => theme.colors.status[status]};
+`;
+
+export const ButtonGoogle = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  background-color: ${theme.colors.primary};
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+`;
+
+export const ButtonContent = styled.Text`
+  font-size: 22px;
+  color: ${theme.colors.text.light};
+  font-family: ${theme.fonts.regular};
+`;
